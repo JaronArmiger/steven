99151a6129abcd622f11e4e4b67e2b386f20249a
JaronArmiger <jaron.armiger@gmail.com>
2020-06-22 17:00:45 -0500

    fix typo in static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:home]
 	def home
 		@main_user = User.first
-		@post = user.posts.build
+		@post = @main_user.posts.build
 		@feed_items = Post.all
 	end
 end
