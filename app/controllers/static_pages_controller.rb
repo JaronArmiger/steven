1612b670a9a2492f500ae102749ff5433c829b46
JaronArmiger <jaron.armiger@gmail.com>
2020-06-22 16:58:06 -0500

    modify blank @post for post_form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:home]
 	def home
-		@post = current_user.posts.build
+		@main_user = User.first
+		@post = user.posts.build
 		@feed_items = Post.all
 	end
 end
