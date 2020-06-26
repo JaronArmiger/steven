36ddd7ad61632b02f9ba8978f7f4755b53ee2091
JaronArmiger <jaron.armiger@gmail.com>
2020-06-26 14:06:14 -0500

    this shit is so fucked up. We'll just have to set shit up better on the next app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
-import 'jquery'
+
 import 'popper.js'
 import 'bootstrap'
 
