774568f6487bb76bd22152e66ba3a9ba4b4da49f
JaronArmiger <jaron.armiger@gmail.com>
2020-06-28 15:08:45 -0500

    fix bug so that navbar is displayed with 20px of top

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,7 +2,8 @@ let prevScrollPos = window.pageYOffset;
 const navbar = document.querySelector('nav');
 window.onscroll = function() {
 	let currentScrollPos = window.pageYOffset;
-	if (prevScrollPos > currentScrollPos) {
+	console.log(currentScrollPos);
+	if (prevScrollPos > currentScrollPos || currentScrollPos < 20) {
 		navbar.style.top = "0";
 	} else {
 		navbar.style.top = '-200px';
