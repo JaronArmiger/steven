cc07203232a670c5a5839329419115d45997409a
JaronArmiger <jaron.armiger@gmail.com>
2020-06-27 20:43:55 -0500

    style log in, sign up pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
 	has_many :comments, as: :commentable
 	belongs_to :user
 	has_one_attached :image
+
+	validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+									 message: "must be valid image format" },
+					 size: { less_than: 5.megabytes,
+					 		 message: "should be less than 5MB" }
+	HEX_REGEX = /\A[#]{1}[\w]{6}\z/
+	validates :font_color, format: { with: HEX_REGEX }
+	validates :body_color, format: { with: HEX_REGEX }
 end
