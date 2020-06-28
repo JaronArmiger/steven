8f3c6fd36a92c10663bbdc257cbbad5e7a303e9d
JaronArmiger <jaron.armiger@gmail.com>
2020-06-28 15:26:14 -0500

    add edit and delete links to _post partial

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,7 +2,6 @@ let prevScrollPos = window.pageYOffset;
 const navbar = document.querySelector('nav');
 window.onscroll = function() {
 	let currentScrollPos = window.pageYOffset;
-	console.log(currentScrollPos);
 	if (prevScrollPos > currentScrollPos || currentScrollPos < 20) {
 		navbar.style.top = "0";
 	} else {
