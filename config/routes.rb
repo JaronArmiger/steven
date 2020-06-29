a1ae04fcff50b707b06c71501dec6371b2044f81
JaronArmiger <jaron.armiger@gmail.com>
2020-06-29 18:49:16 -0500

    sign up path successfully removed from routes while edit_registration still works

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # disable sign up
   # https://stackoverflow.com/questions/39773890/how-to-remove-disable-sign-up-from-devise
-  devise_scope :user do
-  	get '/sign_in' => 'devise/sessions#new' # custom path to login/sign_in
-  	get '/sign_up' => 'devise/registrations#new', as: 'new_user_registration' # custom path to sign up/registration
-  end
 
   devise_for :users, :skip => [:registrations]
-    as :user do
-    	get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    	put 'users' => 'devise/registrations#update', :as => 'user_registration'
-    end
+  as :user do
+    get 'users/edit', to: 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users', to: 'devise/registrations#update', as: 'user_registration'
+  end
   resources :settings, only: [:edit, :update]
   root 'static_pages#home'
   resources :posts
