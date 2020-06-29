8985bc4f9dd682499a7d1407a74545122035c90d
JaronArmiger <jaron.armiger@gmail.com>
2020-06-28 20:35:53 -0500

    JS working! I just put the contents of each file within a function and called that function at the end of the file to get it to run on the page

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,11 +1,14 @@
-let prevScrollPos = window.pageYOffset;
-const navbar = document.querySelector('nav');
-window.onscroll = function() {
-	let currentScrollPos = window.pageYOffset;
-	if (prevScrollPos > currentScrollPos || currentScrollPos < 20) {
-		navbar.style.top = "0";
-	} else {
-		navbar.style.top = '-200px';
+function navbarFunctionality() {
+	let prevScrollPos = window.pageYOffset;
+	const navbar = document.querySelector('nav');
+	window.onscroll = function() {
+		let currentScrollPos = window.pageYOffset;
+		if (prevScrollPos > currentScrollPos || currentScrollPos < 20) {
+			navbar.style.top = "0";
+		} else {
+			navbar.style.top = '-200px';
+		}
+		prevScrollPos = currentScrollPos;
 	}
-	prevScrollPos = currentScrollPos;
 }
+navbarFunctionality();
--- app/assets/javascripts/post_form.js
+++ app/assets/javascripts/post_form.js
@@ -1,43 +1,43 @@
-if (typeof textarea === 'undefined' || textarea === null) {
-    console.log('working');
-}
-const textarea = document.querySelector('textarea');
-console.log('running?');
+function postFormFunctionality() {
+    const textarea = document.querySelector('textarea');
 
-textarea.addEventListener('focus', changeHeight);
-textarea.addEventListener('focus', changeHeight);
+	textarea.addEventListener('focus', changeHeight);
+	textarea.addEventListener('input', changeHeight);
 
-function changeHeight(e) {
-	e.target.style.height = 'inherit';
-	let computed = window.getComputedStyle(e.target);
+	function changeHeight(e) {
+		e.target.style.height = 'inherit';
+		let computed = window.getComputedStyle(e.target);
 
-	var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-         + parseInt(computed.getPropertyValue('padding-top'), 10)
-         + e.target.scrollHeight
-         + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-         + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
-	this.style.height = height + 'px';
-}
-let buttonShowing = false;
-const xButton = document.querySelector('#x-button');
-
-textarea.addEventListener('input', function(e) {
-	if (!buttonShowing) {
-		xButton.style.display = "block";
-		buttonShowing = true;
-	};
-});
-xButton.addEventListener('click', function(e) {
-	textarea.value = '';
-	xButton.style.display = 'none';
-	buttonShowing = false;
-});
-
-const file_field = document.querySelector('#post_image');
-file_field.addEventListener('change', function(e) {
-	const size_in_megabytes = this.files[0].size/1024/1024;
-	if (size_in_megabytes > 5) {
-		alert("Max file size is 5MB. Choose a smaller file dawg");
-		file_field.value = '';
+		var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+	         + parseInt(computed.getPropertyValue('padding-top'), 10)
+	         + e.target.scrollHeight
+	         + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+	         + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+		this.style.height = height + 'px';
 	}
-});
+	let buttonShowing = false;
+	const xButton = document.querySelector('#x-button');
+
+	textarea.addEventListener('input', function(e) {
+		if (!buttonShowing) {
+			xButton.style.display = "block";
+			buttonShowing = true;
+		};
+	});
+	xButton.addEventListener('click', function(e) {
+		textarea.value = '';
+		xButton.style.display = 'none';
+		buttonShowing = false;
+	});
+
+	const file_field = document.querySelector('#post_image');
+	file_field.addEventListener('change', function(e) {
+		const size_in_megabytes = this.files[0].size/1024/1024;
+		if (size_in_megabytes > 5) {
+			alert("Max file size is 5MB. Choose a smaller file dawg");
+			file_field.value = '';
+		}
+	});
+}
+
+postFormFunctionality();
