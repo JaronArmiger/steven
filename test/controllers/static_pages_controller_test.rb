e6dd288f27e7f8672e812eb58db1001fa66baf81
JaronArmiger <jaron.armiger@gmail.com>
2020-06-30 13:20:01 -0500

    write static_pages_controller_test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:alfonso)
+  end
+
+  test "should get home page without post form when no user signed in" do
+  	get root_path
+  	assert_response :success
+  	assert_template 'static_pages/home'
+  	assert_template partial: 'posts/_post'
+  	assert_template partial: 'posts/_post_form', count: 0
+  end
+
+  test "should get home page with post form when user signed in" do
+  	sign_in @user
+  	get root_path
+  	assert_response :success
+  	assert_template 'static_pages/home'
+  	assert_template partial: 'posts/_post'
+  	assert_template partial: 'posts/_post_form'
+  end
 end
