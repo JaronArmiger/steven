2334f83eee43d78f31dd2be99bb7deaaa7b12fcb
JaronArmiger <jaron.armiger@gmail.com>
2020-07-01 10:44:55 -0500

    write setting model test

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class SettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@setting = settings(:one)
+  end
+
+  test "should be valid" do
+  	assert @setting.valid?
+  end
+
+  test "should reject color if it's not in hex format" do
+  	@setting.color = "fuchsia"
+  	assert_not @setting.valid?
+  end
 end
